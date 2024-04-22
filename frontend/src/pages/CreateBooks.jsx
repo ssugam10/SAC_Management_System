cc4f074952c490aecc6ac1760cd97326a11436cd
Vatsal Bhuva <vatsalbhuva11@gmail.com>
2024-04-23 03:52:26 +0530

    create item and view in home page

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,141 +1,108 @@
-import React, { useState } from 'react';
-import BackButton from '../components/BackButton';
-import Spinner from '../components/Spinner';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const CreateItem = () => {
-  const [name, setName] = useState('');
-  const [timeOfIssue, setTimeOfIssue] = useState('');
-  const [needRepairs, setNeedRepairs] = useState('');
-  const [quantity, setQuantity] = useState('');
-  const [remaining, setRemaining] = useState('');
-  const [createdAt, setCreatedAt] = useState('');
-  const [updatedAt, setUpdatedAt] = useState('');
-  const [studentId, setStudentId] = useState('');
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [needRepairs, setNeedRepairs] = useState(false);
+    const [quantity, setQuantity] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
-  const handleSaveBook = () => {
-    const data = {
-      name,
-      timeOfIssue,
-      needRepairs,
-      quantity,
-      remaining,
-      createdAt,
-      updatedAt,
-      studentId
+    const handleSaveBook = () => {
+        const data = {
+            name,
+            repairs: needRepairs,
+            quantity,
+        };
+        setLoading(true);
+        axios
+            .post(`http://localhost:5555/api/item/`, data)
+            .then(() => {
+                setLoading(false);
+                navigate("/");
+            })
+            .catch((err) => {
+                setLoading(false);
+                alert("An error has occurred, please check the console.");
+                console.log(err);
+            });
     };
-    setLoading(true);
-    axios.post(`http://localhost:5555/books`, data)
-      .then(() => {
-        setLoading(false);
-        navigate('/');
-      })
-      .catch(err => {
-        setLoading(false);
-        alert('An error has occurred, please check the console.');
-        console.log(err);
-      });
-  }
 
-  return (
-    <div className='p-8 bg-gray-200'>
-      <BackButton />
-      <h1 className='text-3xl font-bold my-4 text-center'>Create Item</h1>
-      {loading && <Spinner />}
-      <div className='bg-gray-100 flex flex-col border-2 border-black rounded-xl max-w-md mx-auto p-4'>
-        <div className='my-4'>
-          <label htmlFor='name' className='text-lg font-semibold text-gray-800'>Name</label>
-          <input
-            id='name'
-            type='text'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className='border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500'
-          />
+    return (
+        <div className="p-8 h-screen bg-gray-200">
+            <BackButton />
+            <h1 className="text-3xl font-bold my-4 text-center">Create Item</h1>
+            {loading && <Spinner />}
+            <div className="bg-gray-100 flex flex-col border-2 border-black rounded-xl max-w-md mx-auto p-4">
+                <div className="my-4">
+                    <label
+                        htmlFor="name"
+                        className="text-lg font-semibold text-gray-800"
+                    >
+                        Name
+                    </label>
+                    <input
+                        id="name"
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500"
+                    />
+                </div>
+
+                <div className="flex flex-wrap">
+                    <div className="w-full md:w-1/2 my-4 md:pr-2">
+                        <label
+                            htmlFor="quantity"
+                            className="text-lg font-semibold text-gray-800"
+                        >
+                            Quantity
+                        </label>
+                        <input
+                            id="quantity"
+                            type="number"
+                            value={quantity}
+                            onChange={(e) => setQuantity(e.target.value)}
+                            className="border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500"
+                        />
+                    </div>
+
+                    <div className="w-full md:w-1/2 my-4 md:pl-2">
+                        <label
+                            htmlFor="repairds"
+                            className="text-lg font-semibold text-gray-800"
+                        >
+                            Needs repairs?
+                        </label>
+                        <div>
+                            <select
+                                id="repairs"
+                                value={needRepairs}
+                                onChange={(e) => {
+                                    setNeedRepairs(e.target.value);
+                                    console.log(e.target.value);
+                                }}
+                                className="border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500"
+                            >
+                                <option value={false}>No</option>
+                                <option value={true}>Yes</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <button
+                    className="mt-2 mb-2 py-2 px-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
+                    onClick={handleSaveBook}
+                >
+                    Save
+                </button>
+            </div>
         </div>
-        <div className='my-4'>
-          <label htmlFor='timeOfIssue' className='text-lg font-semibold text-gray-800'>Time of Issue</label>
-          <input
-            id='timeOfIssue'
-            type='text'
-            value={timeOfIssue}
-            onChange={(e) => setTimeOfIssue(e.target.value)}
-            className='border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500'
-          />
-        </div>
-        <div className='my-4'>
-          <label htmlFor='needRepairs' className='text-lg font-semibold text-gray-800'>Does it need any repairs?</label>
-          <input
-            id='needRepairs'
-            type='text'
-            value={needRepairs}
-            onChange={(e) => setNeedRepairs(e.target.value)}
-            className='border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500'
-          />
-        </div>
-        <div className='flex flex-wrap'>
-          <div className='w-full md:w-1/2 my-4 md:pr-2'>
-            <label htmlFor='quantity' className='text-lg font-semibold text-gray-800'>Quantity</label>
-            <input
-              id='quantity'
-              type='number'
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-              className='border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500'
-            />
-          </div>
-          <div className='w-full md:w-1/2 my-4 md:pl-2'>
-            <label htmlFor='remaining' className='text-lg font-semibold text-gray-800'>Remaining</label>
-            <input
-              id='remaining'
-              type='number'
-              value={remaining}
-              onChange={(e) => setRemaining(e.target.value)}
-              className='border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500'
-            />
-          </div>
-        </div>
-        <div className='flex flex-wrap'>
-          <div className='w-full md:w-1/2 my-4 md:pr-2'>
-            <label htmlFor='createdAt' className='text-lg font-semibold text-gray-800'>Created At</label>
-            <input
-              id='createdAt'
-              type='text'
-              value={createdAt}
-              onChange={(e) => setCreatedAt(e.target.value)}
-              className='border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500'
-            />
-          </div>
-          <div className='w-full md:w-1/2 my-4 md:pl-2'>
-            <label htmlFor='updatedAt' className='text-lg font-semibold text-gray-800'>Updated At</label>
-            <input
-              id='updatedAt'
-              type='text'
-              value={updatedAt}
-              onChange={(e) => setUpdatedAt(e.target.value)}
-              className='border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500'
-            />
-          </div>
-        </div>
-        <div className='my-4'>
-          <label htmlFor='studentId' className='text-lg font-semibold text-gray-800'>Student ID</label>
-          <input
-            id='studentId'
-            type='text'
-            value={studentId}
-            onChange={(e) => setStudentId(e.target.value)}
-            className='border border-gray-400 px-4 py-2 w-full rounded-md focus:outline-none focus:border-blue-500'
-          />
-        </div>
-        <button className='mt-2 mb-2 py-2 px-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600' onClick={handleSaveBook}>
-          Save
-        </button>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
 export default CreateItem;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,86 +1,132 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import moment from "moment";
 
 const Home = () => {
-  const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get("http://localhost:5555/books")
-      .then((response) => {
-        setItems(response.data.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+        setLoading(true);
+        axios
+            .get("http://localhost:5555/api/item")
+            .then((response) => {
+                setItems(response.data.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoading(false);
+            });
+    }, []);
 
-  return (
-    <div className="p-4 bg-gray-100" style={{color:"#333"}}>
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-4xl font-bold">Item List</h1>
-        <Link to="/books/create">
-          <MdOutlineAddBox className="mr-5 mt-5 text-blue-500 text-5xl hover:text-blue-700 cursor-pointer" title="Add New Book" />
-        </Link>
-      </div>
-  
-      {loading ? (  
-        <Spinner />
-      ) : (
-        <table className="w-full">
-          <thead>
-            <tr className=" text-white" style={{backgroundColor:"#333"}}>
-              <th className="px-4 py-2">ID</th>
-              <th className="px-4 py-2">Name</th>
-              <th className="px-4 py-2">Time of Issue</th>
-              <th className="px-4 py-2">Need repairs?</th>
-              <th className="px-4 py-2">Quantity</th>
-              <th className="px-4 py-2">Remaining</th>
-              <th className="px-4 py-2">Created At?</th>
-              <th className="px-4 py-2">Updated At?</th>
-              <th className="px-4 py-2">Student ID</th>
-              <th className="px-4 py-2">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, index) => (
-              <tr key={item._id} className={index % 2 === 0 ? "" : "bg-gray-200 text-gray-800"}>
-                <td className="px-4 py-2 text-center">{index + 1}</td>
-                <td className="px-4 py-2 text-center">{item.name}</td>
-                <td className="px-4 py-2 text-center">{item.timeOfIssue}</td>
-                <td className="px-4 py-2 text-center">{item.needRepairs}</td>
-                <td className="px-4 py-2 text-center">{item.quantity}</td>
-                <td className="px-4 py-2 text-center">{item.remaining}</td>
-                <td className="px-4 py-2 text-center">{item.createdAt}</td>
-                <td className="px-4 py-2 text-center">{item.updatedAt}</td>
-                <td className="px-4 py-2 text-center">{item.studentId}</td>
-                <td className="px-4 py-2 flex items-center justify-center gap-x-4">
-                  <Link to={`/books/details/${item._id}`} className="text-green-600 hover:text-green-800" title="View Details">
-                    <BsInfoCircle className="text-3xl cursor-pointer" />
-                  </Link>
-                  <Link to={`/books/edit/${item._id}`} className="text-yellow-600 hover:text-yellow-800" title="Edit Item">
-                    <AiOutlineEdit className="text-3xl cursor-pointer" />
-                  </Link>
-                  <Link to={`/books/delete/${item._id}`} className="text-red-600 hover:text-red-800" title="Delete Item">
-                    <MdOutlineDelete className="text-3xl cursor-pointer" />
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
-  );  
+    return (
+        <div className="p-4 bg-gray-100" style={{ color: "#333" }}>
+            <div className="flex justify-between items-center mb-8">
+                <h1 className="text-4xl font-bold">Item List</h1>
+                <Link to="/books/create">
+                    <MdOutlineAddBox
+                        className="mr-5 mt-5 text-blue-500 text-5xl hover:text-blue-700 cursor-pointer"
+                        title="Add New Book"
+                    />
+                </Link>
+            </div>
+
+            {loading ? (
+                <Spinner />
+            ) : (
+                <table className="w-full">
+                    <thead>
+                        <tr
+                            className=" text-white"
+                            style={{ backgroundColor: "#333" }}
+                        >
+                            <th className="px-4 py-2">ID</th>
+                            <th className="px-4 py-2">Name</th>
+                            <th className="px-4 py-2">Time of Issue</th>
+                            <th className="px-4 py-2">Need repairs?</th>
+                            <th className="px-4 py-2">Quantity</th>
+                            <th className="px-4 py-2">Remaining</th>
+                            <th className="px-4 py-2">Created At?</th>
+                            <th className="px-4 py-2">Updated At?</th>
+                            <th className="px-4 py-2">Student ID</th>
+                            <th className="px-4 py-2">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item, index) => (
+                            <tr
+                                key={item._id}
+                                className={
+                                    index % 2 === 0
+                                        ? ""
+                                        : "bg-gray-200 text-gray-800"
+                                }
+                            >
+                                <td className="px-4 py-2 text-center">
+                                    {index + 1}
+                                </td>
+                                <td className="px-4 py-2 text-center">
+                                    {item.name}
+                                </td>
+                                <td className="px-4 py-2 text-center">
+                                    {item.timeOfIssue
+                                        ? moment(item.timeOfIssue).format("lll")
+                                        : "-"}
+                                </td>
+                                <td className="px-4 py-2 text-center">
+                                    {item.needRepairs ? "Yes" : "No"}
+                                </td>
+                                <td className="px-4 py-2 text-center">
+                                    {item.quantity}
+                                </td>
+                                <td className="px-4 py-2 text-center">
+                                    {item.remaining}
+                                </td>
+                                <td className="px-4 py-2 text-center">
+                                    {moment(item.createdAt).format("lll")}
+                                </td>
+                                <td className="px-4 py-2 text-center">
+                                    {moment(item.updatedAt).format("lll")}
+                                </td>
+                                <td className="px-4 py-2 text-center">
+                                    {item.studentId ? item.studentId : "-"}
+                                </td>
+                                <td className="px-4 py-2 flex items-center justify-center gap-x-4">
+                                    <Link
+                                        to={`/books/details/${item._id}`}
+                                        className="text-green-600 hover:text-green-800"
+                                        title="View Details"
+                                    >
+                                        <BsInfoCircle className="text-3xl cursor-pointer" />
+                                    </Link>
+                                    <Link
+                                        to={`/books/edit/${item._id}`}
+                                        className="text-yellow-600 hover:text-yellow-800"
+                                        title="Edit Item"
+                                    >
+                                        <AiOutlineEdit className="text-3xl cursor-pointer" />
+                                    </Link>
+                                    <Link
+                                        to={`/books/delete/${item._id}`}
+                                        className="text-red-600 hover:text-red-800"
+                                        title="Delete Item"
+                                    >
+                                        <MdOutlineDelete className="text-3xl cursor-pointer" />
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    );
 };
 
 export default Home;
